773a9ba1e8938370ee64de5815fd3a8f63812662
Evgeny Chencov <epic.berrr@gmail.com>
2025-06-04 14:50:45 +0200

    Improve performance by offloading heavy computation to Web Worker

---
+++ src/components/WebWorkerSumIterations.ts
@@ -0,0 +1,10 @@
+self.onmessage = (event) => {
+    const { iterations } = event.data;
+    let t = 0;
+    
+    for (let i = 0; i < iterations; i++) {
+        t += i;
+    }
+    
+    self.postMessage({ result: t });
+};
